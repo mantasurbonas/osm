df273625447485b550c8e8339d8ca7a93e2b55e2
rytis.jukna@gmail.com <rytis.jukna@gmail.com>
2019-10-20 16:08:24 +0300

    unified API for SelectQuery and JoinQuery

--- src/main/java/gugit/osm/jdbctemplate/JoinQuery.java
+++ src/main/java/gugit/osm/jdbctemplate/JoinQuery.java
@@ -4,13 +4,13 @@ import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.sql.Types;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.RowCallbackHandler;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import gugit.osm.OSM;
@@ -24,7 +24,7 @@ public class JoinQuery<T> {
 	private NamedParameterJdbcTemplate jdbc;
 	private OSM osm;
 	private RowCallbackHandlerImpl rowCallbackHandler = new RowCallbackHandlerImpl();
-	private Map<String, Object> queryParams = new HashMap<String, Object>();
+	private MapSqlParameterSource queryParams = new MapSqlParameterSource();
 
 	private static final Logger logger = LoggerFactory.getLogger(JoinQuery.class);
 	
@@ -44,35 +44,63 @@ public class JoinQuery<T> {
 		return this;
 	}
 
-	public JoinQuery<T> setInt(String paramName, Integer paramValue) {
-		this.queryParams.put(paramName, paramValue);
-		return this;
-	}
+    public JoinQuery<T> setInt(String paramName, Integer paramValue) {
+        if (paramValue != null)
+            this.queryParams.addValue(paramName, paramValue);
+        else
+            this.queryParams.addValue(paramName, null, Types.INTEGER);
+        return this;
+    }
 
-	public JoinQuery<T> setString(String paramName, String paramValue) {
-		this.queryParams.put(paramName, paramValue);
-		return this;
-	}
-	
-	public JoinQuery<T> setTimestamp(String paramName, Timestamp paramValue) {
-		this.queryParams.put(paramName, paramValue);
-		return this;
-	}
+    public JoinQuery<T> setInts(String paramName, Collection<Integer> paramValues){
+        this.queryParams.addValue(paramName, paramValues);
+        return this;
+    }
+    
+    public JoinQuery<T> setString(String paramName, String paramValue) {
+        if (paramValue != null)
+            this.queryParams.addValue(paramName, paramValue);
+        else
+            this.queryParams.addValue(paramName, null, Types.VARCHAR);
+        return this;
+    }
+    
+    public JoinQuery<T> setStrings(String paramName, Collection<String> paramValue){
+        this.queryParams.addValue(paramName, paramValue);
+        return this;
+    }
+    
+    public JoinQuery<T> setTimestamp(String paramName, Timestamp paramValue) {
+        if (paramValue != null)
+            this.queryParams.addValue(paramName, paramValue);
+        else
+            this.queryParams.addValue(paramName, null, Types.TIMESTAMP);
+        return this;
+    }
 
-	public JoinQuery<T> setDate(String paramName, Date paramValue) {
-		this.queryParams.put(paramName, paramValue);
-		return this;
-	}
-	
-	public JoinQuery<T> setBoolean(String paramName, Boolean paramValue) {
-		this.queryParams.put(paramName, paramValue);
-		return this;
-	}
+    public JoinQuery<T> setDate(String paramName, Date paramValue) {
+        if (paramValue != null)
+            this.queryParams.addValue(paramName, paramValue);
+        else
+            this.queryParams.addValue(paramName, null, Types.DATE);
+        return this;
+    }
 
-	public JoinQuery<T> setDouble(String paramName, Double paramValue) {
-		this.queryParams.put(paramName, paramValue);
-		return this;
-	}
+    public JoinQuery<T> setBoolean(String paramName, Boolean paramValue) {
+        if (paramValue != null)
+            this.queryParams.addValue(paramName, paramValue);
+        else
+            this.queryParams.addValue(paramName, null, Types.BOOLEAN);
+        return this;
+    }
+
+    public JoinQuery<T> setDouble(String paramName, Double paramValue) {
+        if (paramValue != null)
+            this.queryParams.addValue(paramName, paramValue);
+        else
+            this.queryParams.addValue(paramName, null, Types.DOUBLE);
+        return this;
+    }
 	
 	private class RowCallbackHandlerImpl implements RowCallbackHandler{
 		public void processRow(ResultSet rs) throws SQLException {
